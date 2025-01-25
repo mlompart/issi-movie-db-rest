72dd911735d72f9864b108e3a0b831c3112fc258
mlompart <mateuszlompart@gmail.com>
2025-01-25 11:55:22 +0100

    Add support for authors

--- main.py
+++ main.py
@@ -34,3 +34,28 @@ def delete_movie(movie_id: int):
         raise HTTPException(status_code=404, detail="Movie not found")
     db_movie.delete_instance()
     return db_movie
+
+@app.get("/actors/", response_model=List[schemas.Actor])
+def get_movies():
+    return list(models.Actor.select())
+
+@app.get("/actors/{actor_id}", response_model=schemas.Actor)
+def get_movie(actor_id: int):
+    db_actor = models.Actor.filter(models.Actor.id == actor_id).first()
+    if db_actor is None:
+        raise HTTPException(status_code=404, detail="Actor not found")
+    return db_actor
+
+@app.post("/actors/", response_model=schemas.Actor)
+def add_actor(actor: schemas.ActorBase):
+    actor = models.Actor.create(**actor.dict())
+    return actor
+
+@app.delete("/actors/{actor_id}", response_model=schemas.Actor)
+def delete_actor(actor_id: int):
+    db_actor = models.Actor.filter(models.Actor.id == actor_id).first()
+    if db_actor is None:
+        raise HTTPException(status_code=404, detail="Actor not found")
+    db_actor.delete_instance()
+    return db_actor
+
